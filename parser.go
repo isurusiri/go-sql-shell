9f9e889d4210744ddaa26fa4fd6748da74adc99c
isurusiri <isuruanatomy@gmail.com>
2020-05-16 09:06:40 +0530

    updates to the parser logic

---
+++ parser.go
@@ -0,0 +1,83 @@
+package gosqlshell
+
+import "fmt"
+
+func tokenFromKeyword(k keyword) token {
+	return token{
+		kind:  keywordKind,
+		value: string(k),
+	}
+}
+
+func tokenFromSymbol(s symbol) token {
+	return token{
+		kind:  symbolKind,
+		value: string(s),
+	}
+}
+
+// Parser represents the parser itself ;)
+type Parser struct {
+	HelpMessagesDisabled bool
+}
+
+func (p Parser) helpMessage(tokens []*token, cursor uint, msg string) {
+	if p.HelpMessagesDisabled {
+		return
+	}
+
+	var c *token
+	if cursor+1 < uint(len(tokens)) {
+		c = tokens[cursor+1]
+	} else {
+		c = tokens[cursor]
+	}
+
+	fmt.Printf("[%d,%d]: %s, near: %s\n", c.loc.line, c.loc.col, msg, c.value)
+}
+
+func (p Parser) parseTokenKind(tokens []*token, initialCursor uint, kind tokenKind) (*token, uint, bool) {
+	cursor := initialCursor
+
+	if cursor >= uint(len(tokens)) {
+		return nil, initialCursor, false
+	}
+
+	current := tokens[cursor]
+	if current.kind == kind {
+		return current, cursor + 1, true
+	}
+
+	return nil, initialCursor, false
+}
+
+func (p Parser) parseToken(tokens []*token, initialCursor uint, t token) (*token, uint, bool) {
+	cursor := initialCursor
+
+	if cursor >= uint(len(token)) {
+		return nil, initialCursor, false
+	}
+
+	if p := tokens[cursor]; t.equals(p) {
+		return p, cursor + 1, true
+	}
+
+	return nil, initialCursor, false
+}
+
+func (p Parser) parseLiteralExpression(tokens []*token, initialCursor uint) (*expression, uint, bool) {
+	cursor := initialCursor
+
+	kinds := []tokenKind{identifierKind, numericKind, stringKind, boolKind}
+	for _, kind := range kinds {
+		t, newCursor, ok := p.parseTokenKind(tokens, cursor, kind)
+		if ok {
+			return &expression{
+				literal: t,
+				kind:    literalKind,
+			}, newCursor, true
+		}
+	}
+
+	return nil, initialCursor, false
+}
